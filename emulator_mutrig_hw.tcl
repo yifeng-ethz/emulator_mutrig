5fa6750cf6db28acd2a0ca53345b928b0031b4d2
Yifeng Wang <yifenwan@phys.ethz.ch>
2026-04-27 13:29:43 +0200

    Update firmware build and toolkit paths

--- emulator_mutrig_hw.tcl
+++ emulator_mutrig_hw.tcl
@@ -54,7 +54,7 @@ set_module_property VERSION                 $VERSION_STRING_DEFAULT_CONST
 set_module_property DESCRIPTION             "MuTRiG Emulator Mu3e IP Core"
 set_module_property GROUP                   "Mu3e Emulators/Modules"
 set_module_property AUTHOR                  "Yifeng Wang"
-set_module_property ICON_PATH               ../quartus_system/misc/logo/mu3e_logo.png
+set_module_property ICON_PATH               ../firmware_builds/misc/logo/mu3e_logo.png
 set_module_property INTERNAL                false
 set_module_property OPAQUE_ADDRESS_MAP      true
 set_module_property INSTANTIATE_IN_SYSTEM_MODULE true
